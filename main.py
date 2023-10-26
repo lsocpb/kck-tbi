078f1e16fe09485166d85fcf19dc8a8e98855036
lsocpb <lukasssonik@gmail.com>
2023-10-26 15:50:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+import thesportsdb
+
+def display_menu():
+    print("==============================")
+    print("PIŁKARSKI PRZEWODNIK - MENU")
+    print("==============================")
+    print("1. Informacje o ligach")
+    print("2. Statystyki")
+    print("3. Aktualności")
+    print("4. Ciekawostki")
+    print("5. Quizy piłkarskie")
+    print("6. Ulubione drużyny")
+    print("7. Wyjście")
+    print("==============================")
+
+while True:
+    display_menu()
+    option = input("Wybierz opcję: ")
+    if option == "7":
+        sys.exit(0)
+    elif option == "1":
+        print("Informacje o ligach")
+    elif option == "2":
+        print("Statystyki")
+    elif option == "3":
+        print("Aktualności")
+    elif option == "4":
+        print("Ciekawostki")
+    elif option == "5":
+        print("Quizy piłkarskie")
+    elif option == "6":
+        print("Ulubione drużyny")
+    else:
+        print("Nieprawidłowa opcja")
