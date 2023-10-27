d417c49b5828295e212a2ff8b12bd11908cf3c30
lsocpb <lukasssonik@gmail.com>
2023-10-27 17:39:52 +0200

    menu fixed

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import sys
+import curses
 from football_leagues import get_top_european_leagues
 
-def display_menu():
-    print("==============================")
-    print("PIŁKARSKI PRZEWODNIK - MENU")
-    print("==============================")
-    print("1. Informacje o ligach")
-    print("2. Statystyki")
-    print("3. Aktualności")
-    print("4. Ciekawostki")
-    print("5. Quizy piłkarskie")
-    print("6. Ulubione drużyny")
-    print("7. Wyjście")
-    print("==============================")
+def display_menu(stdscr, selected_row):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+    menu = ["Informacje o ligach", "Statystyki", "Aktualności", "Ciekawostki", "Quizy piłkarskie", "Ulubione drużyny", "Wyjście"]
 
-while True:
-    display_menu()
-    option = input("Wybierz opcję: ")
-    if option == "7":
-        sys.exit(0)
-    elif option == "1":
-        get_top_european_leagues()
-    elif option == "2":
-        print("Statystyki")
-    elif option == "3":
-        print("Aktualności")
-    elif option == "4":
-        print("Ciekawostki")
-    elif option == "5":
-        print("Quizy piłkarskie")
-    elif option == "6":
-        print("Ulubione drużyny")
-    else:
-        print("Nieprawidłowa opcja")
+    for i, option in enumerate(menu):
+        x = w // 2 - len(option) // 2
+        y = h // 2 - len(menu) // 2 + i
+        if i == selected_row:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, option)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, option)
 
+    stdscr.refresh()
 
+def main(stdscr):
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    selected_row = 0
 
+    while True:
+        display_menu(stdscr, selected_row)
+        key = stdscr.getch()
+
+        if key == curses.KEY_DOWN and selected_row < 6:
+            selected_row += 1
+        elif key == curses.KEY_UP and selected_row > 0:
+            selected_row -= 1
+        elif key == 10:  # Enter key
+            if selected_row == 6:
+                sys.exit(0)
+            elif selected_row == 0:
+                get_top_european_leagues()
+            elif selected_row == 1:
+                print("Statystyki")
+            elif selected_row == 2:
+                print("Aktualności")
+            elif selected_row == 3:
+                print("Ciekawostki")
+            elif selected_row == 4:
+                print("Quizy piłkarskie")
+            elif selected_row == 5:
+                print("Ulubione drużyny")
+
+if __name__ == '__main__':
+    curses.wrapper(main)
